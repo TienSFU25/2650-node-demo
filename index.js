cc697bd5d192ac4bb5c05fc415fcd14924dea53a
Tien Vu <tienv2510@gmail.com>
2024-03-18 16:52:42 -0400

    --amend

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   console.log("Received something!");
-  res.send("Hello World 1234!");
+  res.send("Hello World 12345!");
 });
 
 app.listen(port, () => {
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 PATH="/root/.nvm/versions/node/v20.11.1/bin:$PATH"
 cd /home/ec2-user/app
-node index.js
+# node index.js
+docker build -t tienv/test-node
+docker run -d -P --name tien-node-test tienv/test-node
