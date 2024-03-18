803519cbf225491b96ffad32538a9b8f42b99edc
Tien Vu <tienv2510@gmail.com>
2024-03-18 17:10:30 -0400

    --amend

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,4 @@ PATH="/root/.nvm/versions/node/v20.11.1/bin:$PATH"
 cd /home/ec2-user/app
 # node index.js
 docker build -t tienv/test-node .
-docker run -d -P --name tien-node-test tienv/test-node
+docker run -d -p 3000:3000 --name tien-node-test tienv/test-node
